93ba504b10bb7e1449d8911f464397be4b6f2b12
Nagaraju Kankanala <nagaraju.kankanala@ATMECSINLT-1165.local>
2025-01-17 15:54:06 +0530

    staging

--- Bazel_Migrate/Soruce/ViewController.swift
+++ Bazel_Migrate/Soruce/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func forgotPassWordBtnClk(_ sender:UIButton) {
-        print("this resplved ")
+        print("this resplved latest updadate")
     }
 }
 
