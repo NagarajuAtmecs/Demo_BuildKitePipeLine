d39437f46e3162a0162fb14caa1d54b8cb0c6cb4
Nagaraju Kankanala <nagaraju.kankanala@ATMECSINLT-1165.local>
2025-01-17 15:37:30 +0530

    This is local

--- Bazel_Migrate/Soruce/ViewController.swift
+++ Bazel_Migrate/Soruce/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func forgotPassWordBtnClk(_ sender:UIButton) {
-        print("this is i just resplved problem")
+        print("this resplved problem")
     }
 }
 
